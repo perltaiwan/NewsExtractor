dbe287b7bbbd5f6699bdab8309111cfa2167b08e
Kang-min Liu <gugod@gugod.org>
2019-07-27 10:38:52 +0900

    catch connection errors.

--- lib/NewsExtractor.pm
+++ lib/NewsExtractor.pm
@@ -3,6 +3,8 @@ our $VERSION = v0.0.1;
 use Moo;
 
 use Mojo::UserAgent;
+use Mojo::UserAgent::Transactor;
+use Try::Tiny;
 
 use Types::URI qw< Uri >;
 use NewsExtractor::Error;
@@ -13,18 +15,30 @@ has url => ( required => 1, is => 'ro', isa => Uri, coerce => 1 );
 sub download {
     my NewsExtractor $self = shift;
 
-    my $ua = Mojo::UserAgent->new;
-    my $tx = $ua->get( "". $self->url );
-    my $res = $tx->result;
+    my $ua = Mojo::UserAgent->new()->transactor(
+        Mojo::UserAgent::Transactor->new()->name('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:67.0) Gecko/20100101 Firefox/67.0')
+    )->max_redirects(3);
 
     my ($error, $download);
-    if ($res->is_error) {
+
+    my $tx = $ua->get( "". $self->url );
+
+    my $res = try { $tx->result } catch {
         $error = NewsExtractor::Error->new(
             is_exception => 0,
-            message => $res->message,
+            message => $_
         );
-    } else {
-        $download = NewsExtractor::Download->new( tx => $tx );
+    };
+
+    if ($res) {
+        if ($res->is_error) {
+            $error = NewsExtractor::Error->new(
+                is_exception => 0,
+                message => $res->message,
+            );
+        } else {
+            $download = NewsExtractor::Download->new( tx => $tx );
+        }
     }
     return ($error, $download);
 }
