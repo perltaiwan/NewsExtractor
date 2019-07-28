cd17bb42cb988e7876e476d0a7668c4c55d9ebaf
Kang-min Liu <gugod@gugod.org>
2019-07-28 21:27:59 +0900

    favoring utf8::is_utf8()

--- lib/NewsExtractor/Types.pm
+++ lib/NewsExtractor/Types.pm
@@ -5,9 +5,7 @@ use Type::Library -base;
 use Type::Utils -all;
 extends "Types::Standard";
 
-use Encode qw< is_utf8 >;
-
-declare Text   => as "Str",  where { is_utf8($_) };
+declare Text   => as "Str",  where { utf8::is_utf8($_) };
 declare Text1K => as "Text", where { length($_) <= 1024 };
 declare Text4K => as "Text", where { length($_) <= 4096 };
 
