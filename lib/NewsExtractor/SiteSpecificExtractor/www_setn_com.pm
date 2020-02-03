b0869c0c60cf9653ab44a7cce8c93c875817dfac
Kang-min Liu <gugod@gugod.org>
2020-02-04 01:11:02 +0900

    cover one more test cases.
    test case: https://www.setn.com/News.aspx?NewsID=682670

--- lib/NewsExtractor/SiteSpecificExtractor/www_setn_com.pm
+++ lib/NewsExtractor/SiteSpecificExtractor/www_setn_com.pm
@@ -7,7 +7,7 @@ use Importer 'NewsExtractor::TextUtil' => 'normalize_whitespace';
 
 sub journalist {
     my ($self) = @_;
-    my ($txt) = $self->content_text =~ m{\b記者(\p{Letter}+?)／(?:\p{Letter}+?)報導\b};
+    my ($txt) = $self->content_text =~ m{\b記者([\p{Letter}、]+?)／(?:\p{Letter}+?)報導\b};
     return normalize_whitespace($txt);
 }
 
