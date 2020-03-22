5148477da5ad474be1abb5709ff525f8ab9b1661
Kang-min Liu <gugod@gugod.org>
2020-03-22 12:22:45 +0800

    Improve the extraction of journalist from setn.
    test case: https://www.setn.com/News.aspx?NewsID=706627

--- lib/NewsExtractor/SiteSpecificExtractor/www_setn_com.pm
+++ lib/NewsExtractor/SiteSpecificExtractor/www_setn_com.pm
@@ -10,7 +10,7 @@ sub journalist {
     my $content_text = $self->content_text;
     my ($name) = $content_text =~ m{\b記者([\p{Letter}、]+?)／(?:\p{Letter}+?)報導\b};
     unless ($name) {
-	($name) = $content_text =~ m{\b生活中心／(\p{Letter}+?)報導\n};
+	($name) = $content_text =~ m{\b(?:生活|國際)中心／(\p{Letter}+?)報導\n};
     }
     return normalize_whitespace($name);
 }
