e98c350b4c8720fdc96ef6986e098853c83c8fd1
Kang-min Liu <gugod@gugod.org>
2020-05-03 08:14:20 +0800

    Improve the recall of journalist name extraction on SETN.
    Optional whitespaces.
    
    Test case: https://www.setn.com/News.aspx?NewsID=735769

--- lib/NewsExtractor/SiteSpecificExtractor/www_setn_com.pm
+++ lib/NewsExtractor/SiteSpecificExtractor/www_setn_com.pm
@@ -10,7 +10,7 @@ sub journalist {
     my $content_text = $self->content_text;
 
     my @patterns = (
-        qr{\b記者([\p{Letter}、]+?)／(?:\p{Letter}+?)報導\b},
+        qr{\b記者\s*([\p{Letter}、]+?)\s*／\s*(?:\p{Letter}+?)報導\b},
         qr{\b文／([\p{Letter}、]+)\b},
         qr{\b三立準氣象／(\p{Letter}+?)報導\n},
         qr{\b(?:娛樂|財經|政治|鄉民|社會|體育|生活|國際)中心／(\p{Letter}+?)報導\n},
