23ca4b83cdd18d917ec29015e0ca48202e6cfa97
Kang-min Liu <gugod@gugod.org>
2020-07-02 21:39:47 +0800

    fix the format of the offset part.

--- lib/NewsExtractor/SiteSpecificExtractor/www_fountmedia_io.pm
+++ lib/NewsExtractor/SiteSpecificExtractor/www_fountmedia_io.pm
@@ -8,7 +8,7 @@ sub dateline {
     my $dom = $self->dom;
     my $el_date = $dom->at("div.detitle2 > div.cell > div");
     my $el_time = $dom->at("div.detitle2 > div.cell.time");
-    my $dateline = ($el_date->text =~ s/\./-/gr) . "T" . ($el_time->text =~ s/[ap]m/:00+0800/r);
+    my $dateline = ($el_date->text =~ s/\./-/gr) . "T" . ($el_time->text =~ s/[ap]m/:00+08:00/r);
     return $dateline;
 }
 
