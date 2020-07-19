176cb7ba16553ea23c51ef704641a1d98099dae7
Kang-min Liu <gugod@gugod.org>
2020-07-19 19:41:47 +0800

    parse the dateline from n.yam.com

--- lib/NewsExtractor/GenericExtractor.pm
+++ lib/NewsExtractor/GenericExtractor.pm
@@ -62,7 +62,7 @@ sub dateline {
     elsif ($guess = $dom->at("time[itemprop=datePublished][datetime], h1 time[datetime], .func_time time[pubdate], span.time > time.post-published")) {
         $dateline = $guess->attr('datetime');
     }
-    elsif ($guess = $dom->at(".reporter time, span.time, span.viewtime, header.article-desc time, .timeBox .updatetime span, .caption div.label-date, .contents_page span.date, .main-content span.date, .newsin_date, .news .date, .author .date, ul.info > li.date > span:nth-child(2), #newsHeadline span.datetime, article p.date, .post-meta > .icon-clock > span, .article_info_content span.info_time, .content time.page-date, .c_time, .newsContent p.time, .story_bady_info_author span:nth-child(1), div.title > div.time, div.article-meta div.article-date, address.authorInfor time, .entry-meta .date a, .author-links .posts-date, .top_title span.post_time, .node-inner > .submitted > span")) {
+    elsif ($guess = $dom->at(".reporter time, span.viewtime, header.article-desc time, .timeBox .updatetime span, .caption div.label-date, .contents_page span.date, .main-content span.date, .newsin_date, .news .date, .author .date, ul.info > li.date > span:nth-child(2), #newsHeadline span.datetime, article p.date, .post-meta > .icon-clock > span, .article_info_content span.info_time, .content time.page-date, .c_time, .newsContent p.time, .story_bady_info_author span:nth-child(1), div.title > div.time, div.article-meta div.article-date, address.authorInfor time, .entry-meta .date a, .author-links .posts-date, .top_title span.post_time, .node-inner > .submitted > span")) {
         $dateline = $guess->text;
     }
     elsif ($guess = $dom->at("div#articles cite")) {
@@ -105,8 +105,11 @@ sub dateline {
     elsif ($guess = $dom->at('#details_block .left .date, .article_header > .author > span:last-child')) {
         $dateline = normalize_whitespace $guess->text;
     }
-    elsif ($guess = $dom->at('.timebox > .publishtime')) {
-        # howlife.cna.com.tw
+    elsif ($guess = $dom->at(
+        join(','
+             , '.timebox > .publishtime'  # howlife.cna.com.tw
+             , 'div.newsInfo > span.time' # n.yam.com
+        ))) {
         $dateline = parse_dateline_ymdhms($guess->all_text, '+08:00');
     }
 
