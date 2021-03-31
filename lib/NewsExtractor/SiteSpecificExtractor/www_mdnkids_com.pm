bba4224ccb511d2518ad1caa25af7ec5ead1ba3e
Kang-min Liu <gugod@gugod.org>
2021-03-31 21:09:40 +0900

    [mdnkids] bring back some old logic.
    it looks like old articles still contain journalist names in the
    content_text instead of in a dedicate place.

--- lib/NewsExtractor/SiteSpecificExtractor/www_mdnkids_com.pm
+++ lib/NewsExtractor/SiteSpecificExtractor/www_mdnkids_com.pm
@@ -17,11 +17,27 @@ sub dateline {
 
 sub journalist {
     my ($self) = @_;
-    my ($x);
-    if (my $el = $self->dom->at('h2 ~ div.col span')) {
+    my ($x, $el);
+    if ($el = $self->dom->at('h2 ~ div.col span')) {
         $x = $el->all_text() =~ s<\([0-9]{4}/[0-9]{1,2}/[0-9]{1,2}\)\z><>r;
         $x = normalize_whitespace($x);
     }
+    unless ($x) {
+        my $t = $self->content_text;
+
+        my @regexps = (
+            qr{\A(\S+)／\S+報導\n},
+            qr{\A\S*(報導／\S+\s+攝影／\S+)\n},
+            qr{\A\S*(文／\S+\s+圖／\S+)\n},
+            qr{\A\S*(\S{3})\n},
+        );
+
+        for my $re (@regexps) {
+            ($x) = $t =~ /$re/;
+            last if $x;
+        }
+    }
+
     return $x;
 }
 
